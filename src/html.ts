2680c2ea75eeabaf3acb1419425b2e7dc601e4aa
dtinth on MBP M1 <dtinth@spacet.me>
2022-12-12 02:10:36 +0700

    Doc

--- src/html.ts
+++ src/html.ts
@@ -1,7 +1,11 @@
 import escape from 'lodash.escape'
 
 /**
- *
+ * Use the `html` template tag to safely generate HTML.
+ * It returns a `Hypertext` object.
+ * You can use the `toHtml` method to get the HTML code.
+ * Interpolated values are escaped unless they are
+ * @public
  */
 export function html(
   strings: TemplateStringsArray,
@@ -14,8 +18,25 @@ export function html(
   )
 }
 
+/**
+ * The `Hypertext` class is used to represent HTML code.
+ * @public
+ */
 export class Hypertext {
+  /**
+   * @internal
+   */
   constructor(private __html: string) {}
+
+  /**
+   * Create a `Hypertext` object from a value.
+   *
+   * If the value is a string, it is escaped.
+   * If the value is an array, it is recursively converted to HTML.
+   * If the value is an object with a `__html` property, it is used as is.
+   * If the value is `null` or `undefined`, it is converted to an empty string.
+   * Otherwise, it is converted to a string and escaped.
+   */
   static from(html: any): Hypertext {
     if (Array.isArray(html)) {
       return new Hypertext(html.map((x) => Hypertext.from(x).toHtml()).join(''))
@@ -31,13 +52,24 @@ export class Hypertext {
       return new Hypertext(escape(String(html)))
     }
   }
+
+  /**
+   * Get the HTML code.
+   */
   toHtml() {
     return this.__html
   }
 }
 
-export type Html = Hypertext | string | Html[]
+/**
+ * The `Html` type represents anything that can be converted to HTML.
+ * Use this type in your functions to accept HTML code.
+ */
+export type Html = Hypertext | string | number | boolean | Html[]
 
+/**
+ * Convert a value to HTML.
+ */
 export function renderHtml(html: Html): string {
   return Hypertext.from(html).toHtml()
 }
